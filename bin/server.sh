6bd823d90f0ddf4fd4993a6e5aa07087e2280e9e
Benoit MERLE <benoitmerle@fserv.fr>
2024-05-06 10:47:31 +0200

    fix: choosing date from headers

--- bin/server.sh
+++ bin/server.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-#while true
-#do
-#  nc -lvp ${PORT:-80} -e parser-http
-#done
+# https://dev.to/leandronsp/building-a-web-server-in-bash-part-ii-parsing-http-14kg
 
 rm -f /home/ruby/response
 mkfifo /home/ruby/response
 
 function handleRequest() {
   while read line; do
-    echo $line
+#    echo $line
     trline=`echo ${line} | tr -d '[\r\n]'`
 
     if [ -z "${trline}" ]; then
@@ -18,9 +15,17 @@ function handleRequest() {
     fi
 
     HEADLINE_REGEX='(.*?)\s(.*?)\sHTTP.*?'
-
     [[ "${trline}" =~ ${HEADLINE_REGEX} ]] &&
       REQUEST=$(echo $trline | sed -E "s/${HEADLINE_REGEX}/\1 \2/")
+
+    DATE_FROM_REGEX='Date-From:\s(.*?)'
+    [[ "$trline" =~ $DATE_FROM_REGEX ]] &&
+      DATE_FROM=$(echo $trline | sed -E "s/$DATE_FROM_REGEX/\1/")
+
+    DATE_TO_REGEX='Date-To:\s(.*?)'
+    [[ "$trline" =~ $DATE_TO_REGEX ]] &&
+      DATE_TO=$(echo $trline | sed -E "s/$DATE_TO_REGEX/\1/")
+
 
   done
 
@@ -29,7 +34,7 @@ function handleRequest() {
       RESPONSE="HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n$(parser)"
       ;;
     "GET /p")
-      RESPONSE="HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n$(parserPower)"
+      RESPONSE="HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n$(parserPower "${DATE_FROM}" "${DATE_TO}")"
       ;;
     *)
       RESPONSE="HTTP/1.1 404 NotFound\r\n\r\n\r\nNot Found\r\n\r\n"
